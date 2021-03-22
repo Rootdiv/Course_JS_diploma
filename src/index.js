04e876d913d9b38bffb3e48cdf086953d9c12ee1
Rootdiv <rootdiv@mail.ru>
2021-03-22 17:54:44 +0300

    feat: togglePopUp Diploma project

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@
 
 import adaptiveMenu from './modules/adaptiveMenu';
 import selectClub from './modules/selectClub';
+import togglePopUp from './modules/togglePopUp';
 
 adaptiveMenu();
 selectClub();
-
+togglePopUp();
