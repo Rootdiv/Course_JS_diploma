5a27924f499a8dd8eaabc0e813b1ff6d9637ab9f
Rootdiv <rootdiv@mail.ru>
2021-03-24 22:52:14 +0300

    fix: validForm.js Diploma project

--- src/modules/validForm.js
+++ src/modules/validForm.js
@@ -7,21 +7,36 @@ const validForm = () => {
     const target = event.target;
     target.value = target.value.replace(/[^а-яё\s]/gi, '');
   };
+  const formNameBlur = (event) => {
+    const target = event.target;
+    target.value = target.value.split(/\s+/)
+      .map(str => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()).join(' ')
+      .trim();
+  };
   const formePhoneInput = (event) => {
     const target = event.target;
-    target.value = target.value.replace(/[^\+\d]/gi, '');
-    if (target.value.charAt(0) === '+' && target.value.length === 13) {
-      target.value = target.value.substring(0, target.value.length - 1);
+    target.value = target.value.replace(/[^\+\d]/g, '');
+    if (target.value.charAt(0) !== '+' || target.value.charAt(1) !== '7') {
+      target.value = '+7';
     }
+    if (target.value.length > 12) {
+      target.value = target.value.substring(0, 12);
+    }
+  };
+  const formPhoneBlur = (event) => {
+    const target = event.target;
+    target.value = target.value.replace(/^[\+]{1,}/g, '+').replace(/[\+]{1,}$/g, '');
   };
   //Вешаем слушатель по клику на body и внутри вешаем слушатель на нужный инпут с вызовом соответствущей функции
   document.body.addEventListener('click', (event) => {
     const target = event.target;
     if (target.matches('[placeholder="Ваше имя..."]')) {
       target.addEventListener('input', formNameInput);
+      target.addEventListener('blur', formNameBlur);
     } else if (target.matches('[placeholder*="Ваш номер"]')) {
       //Вешаем слушатель на поле плейсхолдер которого содержит подстроку "телефон"
       target.addEventListener('input', formePhoneInput);
+      target.addEventListener('blur', formPhoneBlur);
     }
     //Удалять слушатели не нужно, повторного навешивания нет. Проверено.
   });
