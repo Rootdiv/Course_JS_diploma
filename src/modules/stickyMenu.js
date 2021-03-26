e8903517dbbc84028fad02882c0d984c8c6d5d92
Rootdiv <rootdiv@mail.ru>
2021-03-26 18:34:06 +0300

    fix: stickyMenu Fix promo code Diploma project

--- src/modules/stickyMenu.js
+++ src/modules/stickyMenu.js
@@ -3,12 +3,16 @@
 
 const stickyMenu = () => {
   const menu = document.querySelector('.top-menu');
+  const gift = document.querySelector('.fixed-gift');
+  const menuHeight = menu.offsetHeight + 10;
   const startPos = menu.offsetTop;
   window.addEventListener('scroll', () => {
     if (window.pageYOffset >= startPos && window.innerWidth <= 768) {
+      gift.style.top = menuHeight + 'px';
       menu.style.position = 'fixed';
     } else {
       menu.removeAttribute('style');
+      gift.removeAttribute('style');
     }
   });
 };
